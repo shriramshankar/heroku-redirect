c6ce75fdc6b9e94f6b865fc1dc09fe1fb0ddb822
Shriram <sshankar@salesforce.com>
2018-04-10 11:40:00 -0700

    redirect to s3 (#2)

--- web.js
+++ web.js
@@ -5,7 +5,12 @@ var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
 var port = process.env.PORT || 5000;
 
 app.get('*', function(request, response) {
-  response.redirect(301,newBaseURL + request.url);
+  var redirectUrl = newBaseURL + request.url;
+  // adding this for a permanent redirect to s3 bucket
+  if (request.url.includes('Salesforce-Processor-BCR')) {
+    redirectUrl = 'https://s3-us-west-1.amazonaws.com/sftrusthomecomplianc/docs/Salesforce-Processor-BCR.pdf'
+  }
+  response.redirect(301,redirectUrl);
 });
 
 app.listen(port, function() {
